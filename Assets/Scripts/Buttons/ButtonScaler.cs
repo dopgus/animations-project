c6ecc4a072935da07cfc845f72d33393d9e42329
dopgus <gustavolimadacruz4@gmail.com>
2023-05-25 10:53:29 -0300

    animation project

---
+++ Assets/Scripts/Buttons/ButtonScaler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class ButtonScaler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public float finalScale = 1.2f;
+    public float scaleDuration = .1f;
+
+    private Vector3 defaultScale;
+
+    private Tween _currentTween;
+
+    private void Awake()
+    {
+        defaultScale = transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Enter");
+        _currentTween = transform.DOScale(defaultScale * finalScale, scaleDuration);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Exit");
+        _currentTween.Kill();
+        transform.localScale = defaultScale;
+    }
+}
---
+++ Assets/Scripts/Screen/ScreenBase.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using NaughtyAttributes;
+using DG.Tweening;
+
+namespace Screens
+{
+
+    public enum ScreenType
+    {
+        Panel,
+        Info_Panel,
+        Shop
+    }
+
+    public class ScreenBase : MonoBehaviour
+    {
+        public ScreenType screenType;
+
+        public List<Transform> listOfObjects;
+        public List<Typper> listOfPhrases;
+
+        public Image uiBackground;
+        public bool startHired = false;
+
+        [Header("Animation")]
+        private float animationDuration = .3f;
+        private float delayBetweemObjects = .05f;
+
+        private void Start()
+        {
+            if (startHired)
+            {
+                HideObjects();
+            }
+        }
+
+        [Button]
+
+        public virtual void Show()
+        {
+            Debug.Log("Show");
+        }
+
+        [Button]
+
+        public virtual void Hide()
+        {
+            Debug.Log("Hide");
+        }
+
+        private void HideObjects()
+        {
+            listOfObjects.ForEach(i => i.gameObject.SetActive(false));
+            uiBackground.enabled = false;
+        }
+
+        private void ShowObjects()
+        {
+            for(int i = 0; i < listOfObjects.Count; i++)
+            {
+                var obj = listOfObjects[i];
+
+                obj.gameObject.SetActive(true);
+                obj.DOScale(0, animationDuration).From().SetDelay(i * delayBetweemObjects);
+            }
+            Invoke(nameof(StartType), delayBetweemObjects * listOfObjects.Count);
+            uiBackground.enabled = true;
+        }
+
+        private void StartType()
+        {
+            for (int i = 0; i < listOfObjects.Count; i++)
+            {
+                listOfPhrases[i].StartType();
+            }
+        }
+
+        private void ForceShowObjects()
+        {
+            listOfObjects.ForEach(i => i.gameObject.SetActive(true));
+            uiBackground.enabled = true;
+        }
+    }
+}
